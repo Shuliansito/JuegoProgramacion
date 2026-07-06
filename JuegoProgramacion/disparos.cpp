6aa4da3a5d7abbcfd0d6245d84e00a80c1b73f92
Julian BME <jminoesperon2020@gmail.com>
2026-07-06 01:15:16 -0300

    Bug N003 Fixed

--- JuegoProgramacion/disparos.cpp
+++ JuegoProgramacion/disparos.cpp
@@ -25,6 +25,8 @@ void actualizarBalas(std::string mapa[]) {
         for (int i = 0; i < MAX_BULLETS; i++) {
             if (!bullets[i].activa) continue;
 
+            if (mapa[bullets[i].y][bullets[i].x] == '%') {continue;}
+
             gotoxy(25 + bullets[i].x, 3 + bullets[i].y);
             std::cout << BG << " " << RESET;
 
@@ -33,6 +35,8 @@ void actualizarBalas(std::string mapa[]) {
 
             int nx = bullets[i].x;
             int ny = bullets[i].y;
+
+
 
             if (ny < 0 || ny >= 20 || nx < 0 || nx >= (int)mapa[ny].size()) {
                 bullets[i].activa = false;
