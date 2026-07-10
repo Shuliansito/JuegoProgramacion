767d0a6157b7fbbc9cd26fddf572cac7396283d4
Julian BME <jminoesperon2020@gmail.com>
2026-07-10 03:37:40 -0300

    Saque atributos a clase guardia
    No los voy a necesitar al final, voy a usar otra logica a la que tenia pensada :/

--- JuegoProgramacion/Patrullas.cpp
+++ JuegoProgramacion/Patrullas.cpp
@@ -6,17 +6,12 @@
 Guardia guardia[GUARDIAS_MAX];
 int cantGuardias = 0;
 
-void Guardia_Ini(int x, int y, int x1, int y1, int x2, int y2, int nivel) {
+void Guardia_Ini(int x, int y, int nivel) {
 	Guardia g = guardia[cantGuardias];
 	g.x = x;
 	g.y = y;
-	g.x1 = x1;
-	g.y1 = y1;
-	g.x2 = x2;
-	g.y2 = y2;
 	g.nivelGuardia = nivel;
-	g.dx = (x2 > x1) ? 1 : 0;
-	g.dy = (y2 > y1) ? 1 : 0;
+
 	cantGuardias++;
 }
 
--- JuegoProgramacion/Patrullas.h
+++ JuegoProgramacion/Patrullas.h
@@ -7,10 +7,10 @@ extern int cantGuardias;
 
 class Guardia{
 public:
-	int x, y, x1, y1, x2, y2, dx, dy;
+	int x, y;
 	int nivelGuardia;
 };
 
-void Guardia_Ini(int x, int y, int x1,int y1, int x2, int y2, int nivel);
+void Guardia_Ini(int x, int y, int nivel);
 
 void Guardia_Update(string niveles[][20]);
